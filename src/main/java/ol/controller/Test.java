36a7e8b78cc9c75b097a76653fe3577abc326ee1
Oooqf <2442506654@qq.com>
2020-01-01 13:01:49 +0800

    配置文件及测试control

---
+++ src/main/java/ol/controller/Test.java
@@ -0,0 +1,28 @@
+package ol.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+
+
+
+@Controller
+public class Test {
+
+
+	@RequestMapping("/index")
+	public String showIndex() {
+		return "index";
+	}
+
+}
